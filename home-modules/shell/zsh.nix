c118f9f0cad462c145d3cdbf694fc5a6819acb1e
bobymoby <imbobimobi@gmail.com>
2024-07-05 19:07:39 +0300

    zsh nix-shell plugin

--- home-modules/shell/zsh.nix
+++ home-modules/shell/zsh.nix
@@ -1,9 +1,13 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   options.bobymoby.shell.zsh.enable = lib.mkEnableOption "Enable zsh";
 
   config = lib.mkIf config.bobymoby.shell.zsh.enable {
+    home.file."./.config/custom-ohmyzsh" = {
+      source = ./zsh;
+      recursive = true;
+    };
     programs.zsh = {
       enable = true;
       # enableCompletion = true;
@@ -14,13 +18,27 @@
       };
       oh-my-zsh = {
         enable = true;
-        theme = "robbyrussell";
+        theme = "robbyrussell-nix";
         plugins = [
           "git"
           "sudo"
           "docker"
         ];
+        custom = "$HOME/.config/custom-ohmyzsh";
       };
+
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
     };
   };
 }
