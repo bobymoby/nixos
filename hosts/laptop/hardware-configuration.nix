102c684c6629806f3d8cfe47a8d85a2073e7f57b
Borimir Georgiev <imbobimobi@gmail.com>
2024-07-10 02:51:26 +0300

    mounted windows drives

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -4,38 +4,39 @@
 { config, lib, modulesPath, ... }:
 
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    {
+  fileSystems = {
+    "/" = {
       device = "/dev/disk/by-uuid/60b2e7fe-8847-436c-8e22-eda493228aef";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    {
+    "/boot" = {
       device = "/dev/disk/by-uuid/5C69-D966";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  fileSystems."/windowsC" =
-    {
+    "/windowsC" = {
       device = "/dev/disk/by-uuid/8ACE7041CE702799";
       fsType = "ntfs-3g";
       options = [ "rw" "uid=1000" ];
     };
+  };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/30b9deef-b374-45b7-b85a-f0daf95ba2d3"; }];
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/30b9deef-b374-45b7-b85a-f0daf95ba2d3";
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- hosts/pc/hardware-configuration.nix
+++ hosts/pc/hardware-configuration.nix
@@ -4,25 +4,45 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/cbec95eb-31b3-4a71-9938-8a027124c14f";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/cbec95eb-31b3-4a71-9938-8a027124c14f";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0FC2-9E38";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/0FC2-9E38";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
+
+    "/windows/C" = {
+      device = "/dev/disk/by-uuid/2838BF4A38BF15B2";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" ];
+    };
+
+    "/windows/D" = {
+      device = "/dev/disk/by-uuid/A62639652639379F";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" ];
+    };
+
+    "/windows/E" = {
+      device = "/dev/disk/by-uuid/B688A03C889FF957";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" ];
+    };
+  };
 
   swapDevices = [ ];
 
