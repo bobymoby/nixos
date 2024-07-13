eafd81c17c73902612bbc604d53cdff91e982722
Borimir Georgiev <imbobimobi@gmail.com>
2024-07-13 15:28:54 +0300

    removed waybar script nix shell shebang

--- home-modules/wayland/waybar/nixpkgs-behind/cmp.sh
+++ home-modules/wayland/waybar/nixpkgs-behind/cmp.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bash jq
-
 rev=$(jq -r '.nodes.nixpkgs.locked.rev' $FLAKE/flake.lock)
 link="https://api.github.com/repos/NixOS/nixpkgs/compare/nixos-unstable...$rev"
 resp=$(curl --silent $link | jq -r '.behind_by')
