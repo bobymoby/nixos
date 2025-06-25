d8ea3353b2f3e168902653cdd03c9525a1a0e73d
Borimir Georgiev <imbobimobi@gmail.com>
2025-06-25 21:17:12 +0300

    gsettings fix/neovim setup

--- home-modules/dev/neovim/neovim.nix
+++ home-modules/dev/neovim/neovim.nix
@@ -5,6 +5,11 @@
   ...
 }:
 
+let
+  toLua = str: "lua << EOF\n${str}\nEOF\n";
+  # toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  toLuaFile = file: toLua (builtins.readFile file);
+in
 {
   options.bobymoby.dev.neovim.enable = lib.mkEnableOption "Enable Neovim";
 
@@ -28,8 +33,12 @@
       #   set number relativenumber
       # '';
 
-      extraConfig = lib.fileContents ./init.vim;
-      extraLuaConfig = lib.fileContents ./init.lua;
+      # extraConfig = lib.fileContents ./init.vim;
+      # extraLuaConfig = lib.fileContents ./init.lua;
+
+      extraLuaConfig = ''
+        ${builtins.readFile ./options.lua}
+      '';
 
       viAlias = true;
       vimAlias = true;
@@ -38,14 +47,38 @@
       withNodeJs = true;
       withPython3 = true;
 
-      coc.enable = true;
+      # coc.enable = true;
+
+      # plugins = with pkgs.vimPlugins; [
+      #   lualine-nvim
+      #   dracula-nvim
+      #   nerdtree
+      #   telescope-nvim
+      #   nvim-treesitter.withAllGrammars
+      # ];
 
       plugins = with pkgs.vimPlugins; [
+        {
+          plugin = dracula-nvim;
+          config = "colorscheme dracula";
+        }
         lualine-nvim
-        dracula-nvim
-        nerdtree
-        telescope-nvim
-        nvim-treesitter.withAllGrammars
+        nvim-web-devicons
+        {
+          plugin = nvim-treesitter.withAllGrammars;
+          config = toLuaFile ./plugin/treesitter.lua;
+        }
+        vim-nix
+        {
+          plugin = telescope-nvim;
+          config = toLuaFile ./plugin/telescope.lua;
+        }
+        {
+          plugin = comment-nvim;
+          config = toLua "require(\"Comment\").setup()";
+        }
+
+        telescope-fzf-native-nvim
       ];
     };
   };
