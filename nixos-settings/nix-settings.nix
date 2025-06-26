0dc52b9723cc4a3a0bf0fec85a86044c8b76c931
Borimir Georgiev <imbobimobi@gmail.com>
2025-06-26 08:45:41 +0300

    cuda cache/background changes

--- nixos-settings/nix-settings.nix
+++ nixos-settings/nix-settings.nix
@@ -10,9 +10,17 @@ in
 
   nix = {
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 }
