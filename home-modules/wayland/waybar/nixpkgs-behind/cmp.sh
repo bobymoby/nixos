b8900794d64a4d3dc9ffa1a03bd57b599ff612bb
bobymoby <imbobimobi@gmail.com>
2025-10-17 19:47:40 +0300

    shell script fix

--- home-modules/wayland/waybar/nixpkgs-behind/cmp.sh
+++ home-modules/wayland/waybar/nixpkgs-behind/cmp.sh
@@ -1,4 +1,4 @@
 rev=$(jq -r '.nodes.nixpkgs.locked.rev' $FLAKE/flake.lock)
 link="https://api.github.com/repos/NixOS/nixpkgs/compare/nixos-unstable...$rev"
-resp=$(curl --silent $link | jq -r '.behind_by')
-echo $resp
+
+curl --silent $link | jq -r '.behind_by'
